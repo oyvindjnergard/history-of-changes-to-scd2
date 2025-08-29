2db4e83e6dac4d65d444899a0d52b7df2e59f839
oyvindjnergard <93658952+oyvindjnergard@users.noreply.github.com>
2025-08-29 07:52:28 +0200

    Create scd2.sql

---
+++ scd2.sql
@@ -0,0 +1,41 @@
+CREATE TABLE bronze (
+  id SERIAL PRIMARY KEY,
+  key int,
+  name VARCHAR(100),
+  status VARCHAR(255),
+  extract_day date
+);
+
+INSERT INTO bronze(key, name, status, extract_day) VALUES (1, 'Jon', 'Active', '2025-08-01');
+INSERT INTO bronze(key, name, status, extract_day) VALUES (1, 'John', 'Active', '2025-08-05');
+INSERT INTO bronze(key, name, status, extract_day) VALUES (1, null, 'Deleted', '2025-08-10');
+INSERT INTO bronze(key, name, status, extract_day) VALUES (2, 'Bjarne', 'Active', '2025-08-10');
+
+select * from bronze;
+
+with base as (
+    select
+        key,
+        name,
+        status,
+        extract_day as valid_from,
+        coalesce(
+            lead(extract_day) over (partition by key order by extract_day asc),
+            '9999-12-31'
+        ) as valid_to,
+        case
+            lead(status) over (partition by key order by extract_day asc)
+            when 'Active'
+            then 0
+            else 1
+        end as is_valid
+    from bronze
+)
+select 
+    key, 
+    name, 
+    valid_from, 
+    valid_to, 
+    is_valid
+from base
+where status = 'Active';
